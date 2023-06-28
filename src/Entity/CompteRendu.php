ca5593b28b01063db8f1f36c27e06a29ce30c795
Gilbert ARMENGAUD <gilbert.armengaud@gmail.com>
2023-06-28 11:20:52 +0200

    private ContactRepository $contactsRepository

--- src/Entity/CompteRendu.php
+++ src/Entity/CompteRendu.php
@@ -9,7 +9,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CompteRenduRepository::class)]
-#[ORM\Table('compte_rendu')]
+#[ORM\Table('comptes_rendus')]
 class CompteRendu
 {
     #[ORM\Id]
