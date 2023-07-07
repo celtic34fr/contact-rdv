d14c0bebee0babebf5af97c92508332a6352feb3
Gilbert ARMENGAUD <gilbert.armengaud@gmail.com>
2023-07-07 19:07:14 +0200

    add extension parameter gest type event Cal

---
+++ src/Entity/ParamsCalNature.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Celtic34fr\ContactRendezVous\Entity;
+
+use Celtic34fr\ContactCore\Entity\Parameter;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Celtic34fr\ContactCore\Traits\ParametersEntityTrait;
+use Celtic34fr\ContactCore\Repository\ParameterRepository;
+
+class ParamsCalNature extends Parameter
+{
+    const HEADER = [
+        'name', 'description', 'backgroudColor', 'borderColor', 'textColor'
+    ];
+    const PARAM_CLE = "calNature";
+
+    private ParameterRepository $repository;
+
+    public function __construct(private LifecycleEventArgs $events)
+    {
+        parent::__construct();
+        $this->repository = $events->getObjectManager()->getRepository(Parameter::class);
+    }
+
+    use ParametersEntityTrait;
+
+    public function getName(): mixed
+    {
+        return $this->getItem('name');
+    }
+
+    public function setName(string $name): mixed
+    {
+        return $this->setItem('name', $name);
+    }
+
+    public function getDescription(): mixed
+    {
+        return $this->getItem('description');
+    }
+
+    public function setDescription(string $name): mixed
+    {
+        return $this->setItem('description', $name);
+    }
+
+    public function getBackgroundColor(): string
+    {
+        return $this->getItem('backgroundColor');
+    }
+
+    public function setBackgroundColor(string $backgroundColor)
+    {
+        return $this->setItem('backgroundColor', $backgroundColor);
+    }
+
+    public function getBorderColor(): string
+    {
+        return $this->getItem('borderColor');
+    }
+
+    public function setBorderColor(string $borderColor)
+    {
+        return $this->setItem('borderColor', $borderColor);
+    }
+
+    public function getTextColor(): string
+    {
+        return $this->getItem('textColor');
+    }
+
+    public function setTextColor(string $textColor)
+    {
+        return $this->setItem('textColor', $textColor);
+    }
+}
